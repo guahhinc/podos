4a927af4357b1f724d878d0ad18c7ec50170b11b
Guahh <t66305218@gmail.com>
2026-08-05 18:35:24 +1000

    Update and rename snake.cpp to snake.js

---
+++ snake/snake.js
@@ -0,0 +1,11 @@
+// podOS Dynamic App: Snake
+name = Snake
+type = grid_game
+grid_w = 30
+grid_h = 11
+cell_size = 8
+speed = 200
+color_head = light_purple
+color_body = green
+color_food = red
+score_step = 10
